d073c75213e13d1be753da876317722a03e33bd8
Alexandre Toulios <99737712+Lehzinho@users.noreply.github.com>
2025-03-12 18:39:05 -0300

    first commit setup do projeto

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
+import { HelmetProvider, Helmet } from "react-helmet-async";
+
+export function App() {
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <RouterProvider router={router} />;
+    </HelmetProvider>
+  );
+}
---
+++ src/pages/auth/sign-in.tsx
@@ -0,0 +1,10 @@
+import { Helmet } from "react-helmet-async";
+
+export const Signin = () => {
+  return (
+    <>
+      <Helmet title="Login" />
+      <div>Sign in</div>
+    </>
+  );
+};
