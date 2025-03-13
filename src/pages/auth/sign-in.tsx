9f5ccc84cdae09ea79f6c64e1eee94fc6b46d042
Alexandre Toulios <99737712+Lehzinho@users.noreply.github.com>
2025-03-13 16:47:06 -0300

    fix commit

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,67 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@radix-ui/react-label";
 import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import { toast } from "sonner";
+import { z } from "zod";
 
-export const Signin = () => {
+const signInForm = z.object({
+  email: z.string().email(),
+});
+
+type SignInForm = z.infer<typeof signInForm>;
+
+export const SignIn = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>();
+  const handleSignIn = async (data: SignInForm) => {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      toast.success("Enviamos un link de autenticação para seu e-mail.", {
+        action: {
+          label: "Reenviar",
+          onClick: () => handleSignIn(data),
+        },
+      });
+    } catch (error) {
+      toast.error("Credenciais inválidos.");
+    }
+    console.log(data);
+  };
   return (
     <>
       <Helmet title="Login" />
-      <div>Sign in</div>
+      <div className="p-8">
+        <Button variant="ghost" asChild className="absolute right-8 top-8">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
+        <div className="flex flex-col justify-center gap-6 w-[350px]">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar painel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parceiro!
+            </p>
+          </div>
+
+          <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu email</Label>
+              <Input id="email" type="email" {...register("email")} />
+            </div>
+            <Button type="submit" disabled={isSubmitting} className="w-full">
+              Acessar painel
+            </Button>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
