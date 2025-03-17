4c7e5899764c9755aec0f5a95697371d6ffa8774
Alexandre Toulios <99737712+Lehzinho@users.noreply.github.com>
2025-03-17 11:58:20 -0300

    front end styles finished

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -1,9 +1,27 @@
 import { Helmet } from "react-helmet-async";
+import { MonthRevenueCard } from "./month-revenue-card";
+import { MonthOrderAmountCard } from "./month-order-amount-card";
+import { DayOrderAmountCard } from "./day-orders-amount-card";
+import { MonthCanceledOrderAmountCard } from "./month-canceled-orders-amount-card";
+import { RevenueChart } from "./revenue-chart";
+import { PopularProductsChart } from "./popular-products-chart";
 export const Dashboard = () => {
   return (
     <>
       <Helmet title="Dashboard" />
-      <h1>Dashboard</h1>
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
+        <div className="grid grid-cols-4 gap-4">
+          <MonthRevenueCard />
+          <MonthOrderAmountCard />
+          <DayOrderAmountCard />
+          <MonthCanceledOrderAmountCard />
+        </div>
+        <div className="grid grid-cols-9 gap-4">
+          <RevenueChart />
+          <PopularProductsChart />
+        </div>
+      </div>
     </>
   );
 };
